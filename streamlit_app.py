88eb3c9b6a588313abf062ed14e7277845f1f260
Bhaswar Bhattacharyya <sgs.biz27@gmail.com>
2023-07-11 13:25:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,93 @@
-from collections import namedtuple
-import altair as alt
-import math
+import matplotlib.pyplot as plt
+from binance import Client
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+def get_top_returns(timeframe, lookback_period, nlargest):
+    api_key = "698fxbgZzGtMht0gbeAJYsNgd93qX1oNbNSJTjqzzisb6wRvsm1VFhUPn8GlHAZr"
+    api_secret = "Rqi2tpN8SoRqImbFhrxr2KGWPzBwU44Iq4yUf7acsq8Zz0kPBXXOX7wAuQTGpZUy"
+    
+    client = Client(api_key, api_secret)
+    
+    info = client.get_exchange_info()
+    symbols = [x['symbol'] for x in info['symbols']]
+    exclude = ['UP', 'DOWN', 'BEAR', 'BULL']
+    non_lev = [symbol for symbol in symbols if all(excludes not in symbol for excludes in exclude)]
+    
+    relevant = [symbol for symbol in non_lev if symbol.endswith('USDT')]
+    
+    klines = {}
+    
+    with st.spinner("Please Wait while we Fetch Data from the Binance Exchange ..."):
+        progress_bar = st.progress(0)
+        progress_text = st.empty()
+        for i, symbol in enumerate(relevant):
+            klines[symbol] = client.get_historical_klines(symbol, timeframe, f'{lookback_period} min ago UTC')
+            progress_bar.progress((i + 1) / len(relevant))
+            progress_text.text(f"Progress: {i + 1} / {len(relevant)}")
+    
+    returns, symbols = [], []
+    for symbol in relevant:
+        if len(klines[symbol]) > 0:
+            cumret = (pd.DataFrame(klines[symbol])[4].astype(float).pct_change() + 1).prod() - 1
+            returns.append(cumret)
+            symbols.append(symbol)
+    
+    retdf = pd.DataFrame(returns, index=symbols, columns=['ret'])
+    top_returns = retdf.ret.nlargest(nlargest)
+    
+    return top_returns
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def plot_price_chart(symbol):
+    api_key = "698fxbgZzGtMht0gbeAJYsNgd93qX1oNbNSJTjqzzisb6wRvsm1VFhUPn8GlHAZr"
+    api_secret = "Rqi2tpN8SoRqImbFhrxr2KGWPzBwU44Iq4yUf7acsq8Zz0kPBXXOX7wAuQTGpZUy"
+    
+    client = Client(api_key, api_secret)
+    
+    klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1HOUR, "1 day ago UTC")
+    df = pd.DataFrame(klines, columns=["Open Time", "Open", "High", "Low", "Close", "Volume", "Close Time", "Quote Asset Volume", "Number of Trades", "Taker Buy Base Asset Volume", "Taker Buy Quote Asset Volume", "Ignore"])
+    df["Open Time"] = pd.to_datetime(df["Open Time"], unit="ms")
+    df.set_index("Open Time", inplace=True)
+    
+    fig, ax = plt.subplots()
+    ax.plot(df["Close"])
+    ax.set(xlabel="Time", ylabel="Price", title=f"{symbol} Price Chart")
+    ax.grid()
+    
+    return fig
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+def main():
+    st.title("Profitable USDT-Pair Finder")
+    st.subheader("Based on Data from Binance Exchange")
+    st.divider()
+    
+    a,b,c = st.columns(3)
+    with a:
+        timeframe = st.radio("Select timeframe (minutes)", ['1m', '5m', '15m', '30m', '1h'])    
+    with b:
+        lookback_period = st.radio("Select lookback period (minutes)", [30, 60, 180, 360, 1440])        
+    with c:
+        nlargest = st.radio("Select number of pairs to display", [5, 10, 15, 20, 25])  
+    
+    show_table = False
+    
+    if st.button("Get Top Pairs"):
+        top_returns = get_top_returns(timeframe, lookback_period, nlargest)
+        show_table = True
+    
+    if show_table:
+        st.table(top_returns)
+        
+        col1, col2= st.columns([1,5])
+        
+        if col1.button("Reset Data"):
+            show_table = False
+            st.experimental_rerun()
+        
+        if top_returns.shape[0] > 0:
+            top_symbol = top_returns.index[0]
+            fig = plot_price_chart(top_symbol)
+            col2.pyplot(fig)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    main()
